8587ed32d734e6ec230605b74ac6f551a35be5b0
Gabriel Gonçalves <gabrielqgoncalves26@gmail.com>
2023-08-16 17:47:00 -0300

    Update: update constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,51 +1,23 @@
-export const initialCards = [
-  {
-    title: "Vale de Yosemite",
-    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
-    alt: "Um Rio envolto de árvores com uma montanha ao fundo",
-  },
-  {
-    title: "Lago Louise",
-    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
-    alt: "Uma lagoa com montanhas geladas ao fundo",
-  },
-  {
-    title: "Montanhas Carecas",
-    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
-    alt: "Paisagem de montanhas com um pôr do sol ao fundo",
-  },
-  {
-    title: "Latemar",
-    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
-    alt: "céu estrelado com uma grande montanha pontiaguda",
-  },
-  {
-    title: "Parque Nacional da Vanoise ",
-    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
-    alt: "lago que reflete uma montanha na paisagem",
-  },
-  {
-    title: "Lago di Braies",
-    url: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
-    alt: "um pequeno porto com barquinhos no lago e duas grandes montanhas cobertas em neve ao fundo",
-  },
-];
-
 export const cardSelector = ".elements__table"
 export const popup = document.querySelector(".popup");
 export const closePopupButton = document.querySelector(".popup__close-button");
+export const closeConfirmationButton = document.querySelector(".edit-profile__close-button");
 export const saveButton = document.querySelector(".popup__save-button");
 export const inputName = document.querySelector(".popup__input-name");
 export const inputAbout = document.querySelector(".popup__input-about");
+export const inputUrlProfilePic = document.querySelector(".edit-profile__input-url");
 
 export const transparentContainer = document.querySelector(
   ".container__semitransparent"
 );
 
 export const editButton = document.querySelector(".profile__edit-button");
+export const editProfileButton = document.querySelector(".profile__image_edit-button");
 export const addButton = document.querySelector(".profile__add-button");
+export const profileId = '';
 export const profileName = document.querySelector(".profile__title");
 export const profileAbout = document.querySelector(".profile__text");
+export const profileImage = document.querySelector(".profile__image");
 
 export const cardTemplate = document.querySelector("#card__template").content;
 export const cardElement = cardTemplate.querySelector(".element").cloneNode(true);
@@ -53,6 +25,7 @@ export const cards = document.querySelector(".elements__table");
 
 export const local = document.querySelector(".location");
 export const createButton = document.querySelector(".location__create-button");
+export const saveProfilePicture = document.querySelector(".edit-profile__button");
 export const closeLocalButton = document.querySelector(".location__close-button");
 
 export const imageContainer = document.querySelector(".image");
